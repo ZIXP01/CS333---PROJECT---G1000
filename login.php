9ede33b37e612530dfb11523ec8e16f3c1769528
ZixSec <156583439+ZIXP01@users.noreply.github.com>
2025-12-18 20:14:54 +0300

    login.php

--- login.php
+++ login.php
@@ -28,8 +28,8 @@ $err = 'Invalid credentials.';
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Login - ITCS333</title>
 <link rel="stylesheet" href="/public/css/styles.css">
 </head>
@@ -48,4 +48,5 @@ $err = 'Invalid credentials.';
 </main>
 <?php include __DIR__ . '/../includes/footer.php'; ?>
 </body>
+
 </html>
