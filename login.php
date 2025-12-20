6bd3e449d581588eba49febae1df10a564def842
ZixSec <156583439+ZIXP01@users.noreply.github.com>
2025-12-20 09:23:38 +0300

    login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Login - ITCS333</title>
+<title>Login</title>
 <link rel="stylesheet" href="/public/css/styles.css">
 </head>
 <body>
@@ -47,3 +47,4 @@ $err = 'Invalid credentials.';
 }
 }
 ?>
+
